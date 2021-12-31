726b9fb33a4941fb1a950718ae9df7b8598f6037
General Failure <gf59ru@live.com>
2021-12-31 14:10:37 +0200

    Initial Commit

---
+++ RickAndMorty test job/RickAndMorty_test_jobApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMorty_test_jobApp.swift
+//  RickAndMorty test job
+//
+//  Created by Nikolai Borovennikov on 31.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMorty_test_jobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
